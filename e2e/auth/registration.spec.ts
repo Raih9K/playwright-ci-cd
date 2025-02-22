c200b7d236bbec6f357edaf5a1c83ecc4855f2f4
Raihan Khan <71407079+Raih9K@users.noreply.github.com>
2025-02-22 23:32:59 +0600

    reg

--- e2e/auth/registration.spec.ts
+++ e2e/auth/registration.spec.ts
@@ -1,18 +1,23 @@
 import { test, expect } from '@playwright/test';
 import testData from '../test-data/testData.json';
 
-test.describe('Tests Suite name', () => {
+test('Positive Test - Valid Login', async ({ page }) => {
+    const { email, password } = testData.validLogin;
 
-    test('test name', async ({ page }) => {
-        // Test steps
-        // 1. Navigate to the registration page
-        // 2. Enter valid registration details
-        // 3. Click the register button
-        // 4. Validate the successful registration message
-        // 5. Validate the user is redirected to the login page
-        // 6. Validate the user can log in with the registered credentials
-        // 7. Validate the user is redirected to the dashboard page
-        // 8. Log out and validate the user is redirected to the login pag
-    }); 
+    // Navigate to login page
+    await page.goto('https://admin.dev.myqbits.com/');
+    console.log("Navigated to login page");
+
+    // Enter email and password
+    await page.getByRole('textbox', { name: 'Enter your email or phone' }).fill(email);
+    await page.getByRole('textbox', { name: 'Enter your password' }).fill(password);
+    console.log("Entered valid credentials");
+
+    // Click login button
+    await page.getByRole('button', { name: 'Log in' }).click();
+    console.log("Clicked login button");
+
+    // Validate successful login
+    await expect(page.getByRole('alert')).toContainText('You\'re successfully logged in!');
+    console.log("Login successful - Dashboard loaded");
 });
-    
