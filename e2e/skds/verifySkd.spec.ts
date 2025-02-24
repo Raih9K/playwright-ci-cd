4aa04608b4bd460246ec183cf21d5e89d5e19346
Raihan Khan <71407079+Raih9K@users.noreply.github.com>
2025-02-24 23:49:08 +0600

    --

--- e2e/skds/verifySkd.spec.ts
+++ e2e/skds/verifySkd.spec.ts
@@ -2,14 +2,13 @@ import { test, expect } from '@playwright/test';
 import testData from '../test-data/testData.json';
 
 test.describe.serial('SKD Verification', () => {
-
   test('Verify SKD', async ({ page }) => {
     await page.getByRole('link', { name: 'Skds' }).click();
     await page.locator('a').filter({ hasText: 'SKD List' }).click();
-  
+
     const verifySKD = async (rowName) => {
       const row = page.getByRole('row', { name: rowName });
-  
+
       if (await row.isVisible()) {
         await row.getByRole('button').first().click();
         await page.getByRole('textbox', { name: 'Remark' }).fill('tested');
@@ -19,7 +18,8 @@ test.describe.serial('SKD Verification', () => {
         await page.screenshot({ path: `screenshots/${rowName.replace(/\s/g, '_')}.png`, fullPage: true });
       }
     };
-  
+
+
     // 🔥 Calling function for specific SKD items
     await verifySKD('1 Test SKD Test SKD Type true');
   });
