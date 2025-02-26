4e373b1e0d7a9aa85ee3ea15259c0c7e2d442391
raihan-isharify <raihan.isharifyltd@GMAIL.COM>
2025-02-26 10:53:22 +0600

    Update verifySkd.spec.ts

--- e2e/skds/verifySkd.spec.ts
+++ e2e/skds/verifySkd.spec.ts
@@ -2,14 +2,38 @@ import { test, expect } from '@playwright/test';
 import testData from '../test-data/testData.json';
 
 test.describe.serial('SKD Verification', () => {
+  test.beforeEach(async ({ page }) => {
+    // await page.goto('https://admin.dev.myqbits.com/');
+    await page.goto(testData.testServer); //redirect login page
 
+    const emailField = page.getByRole("textbox", {
+      name: "Enter your email or phone",
+    });
+    const passwordField = page.getByRole("textbox", {
+      name: "Enter your password",
+    });
+    const loginButton = page.getByRole("button", { name: "Log in" });
+
+    if ((await emailField.isVisible()) && (await passwordField.isVisible())) {
+      await emailField.fill(testData.validLogin.email);
+      await passwordField.fill(testData.validLogin.password);
+      await loginButton.click();
+    } else {
+      console.log("Login fields are not visible.");
+      await page.screenshot({ path: "error_login_fields.png" });
+    }
+
+    await expect(page.getByRole("alert")).toContainText(
+      "You're successfully logged in!"
+    );
+  });
   test('Verify SKD', async ({ page }) => {
     await page.getByRole('link', { name: 'Skds' }).click();
     await page.locator('a').filter({ hasText: 'SKD List' }).click();
-  
+
     const verifySKD = async (rowName) => {
       const row = page.getByRole('row', { name: rowName });
-  
+
       if (await row.isVisible()) {
         await row.getByRole('button').first().click();
         await page.getByRole('textbox', { name: 'Remark' }).fill('tested');
@@ -19,7 +43,7 @@ test.describe.serial('SKD Verification', () => {
         await page.screenshot({ path: `screenshots/${rowName.replace(/\s/g, '_')}.png`, fullPage: true });
       }
     };
-  
+
     // 🔥 Calling function for specific SKD items
     await verifySKD('1 Test SKD Test SKD Type true');
   });
